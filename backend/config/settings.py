7faee4261c73b4684a4f9d520bf753553c60069f
TheAyushThakur <ayushthakur6005@gmail.com>
2026-02-14 20:00:18 +0530

    cors allowed origins in settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -153,8 +153,6 @@ MEDIA_ROOT = BASE_DIR / 'media'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
 ]
